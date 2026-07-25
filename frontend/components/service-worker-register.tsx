59c95841b9d6150326a3ba7c0a7a1f49b0a1bfff
evinvk <evinvk18@gmail.com>
2026-07-26 00:57:35 +0530

    feat: add Monetag service worker verification to sw.js with build patch script

--- frontend/components/service-worker-register.tsx
+++ frontend/components/service-worker-register.tsx
@@ -8,10 +8,6 @@ export function ServiceWorkerRegister() {
       navigator.serviceWorker.getRegistration().then((reg) => {
         if (reg) reg.update().catch(() => {});
       });
-
-      navigator.serviceWorker.getRegistration("/monetag-sw.js").catch(() => {
-        navigator.serviceWorker.register("/monetag-sw.js", { scope: "/" }).catch(() => {});
-      });
     }
   }, []);
   return null;
